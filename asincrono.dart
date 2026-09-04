54c43bd73a41ead675e4f01294df92154fea4fdd
CalikeSYST <layoncito2020@gmail.com>
2026-09-04 11:32:40 -0500

    se creo los modos asincronos y sincronos con dart

---
+++ asincrono.dart
@@ -0,0 +1,15 @@
+// Funciones avanzados
+
+//FUTURE
+Future<String> prepararPedido() {
+  return Future.delayed(Duration(seconds: 3), () => 'Pedido preparado'); 
+}
+
+void main(){
+  print("Pedido recibido");
+  prepararPedido().then((pedido) {
+    print(pedido);
+  });
+  print("El mozo sigue Trabjando");
+
+}
--- sincrono.dart
+++ sincrono.dart
@@ -1,21 +1,3 @@
-/*
-// Funciones avanzados
-
-//FUTURE
-Future<String> prepararPedido() {
-  return Future.delayed(Duration(seconds: 3), () => 'Pedido preparado'); 
-}
-
-void main(){
-  print("Pedido recibido");
-  prepararPedido().then((pedido) {
-    print(pedido);
-  });
-  print("El mozo sigue Trabjando");
-
-}
-*/
-
 //FUTURE ASYNC Y AWAIT FUTURE.DELAYED
 Future<String> buscarPersona() async {
   //Simula una petición
